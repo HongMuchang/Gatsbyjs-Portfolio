0249183f922e7a05b536ad25d9576cf5544a40f0
HongMuchan <muchang0824@gmail.com>
2020-08-23 06:55:14 +0900

    form追加

--- src/components/Main.js
+++ src/components/Main.js
@@ -110,14 +110,14 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact(Email)</h2>
-          <form method="post" netlify>
+          <form name="contact" method="POST" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input type="email" name="email" id="email" />
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
